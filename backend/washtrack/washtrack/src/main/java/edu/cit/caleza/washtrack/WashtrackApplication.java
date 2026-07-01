5ec6ccdbd3b880c38b949e5221e74a8b4068a189
kenrudson <calezaaken@gmail.com>
2026-07-01 16:32:01 +0800

    chore: scaffold Spring Boot backend project

---
+++ backend/washtrack/washtrack/src/main/java/edu/cit/caleza/washtrack/WashtrackApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.caleza.washtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashtrackApplication.class, args);
+	}
+
+}
---
+++ backend/washtrack/washtrack/src/test/java/edu/cit/caleza/washtrack/WashtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.caleza.washtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
